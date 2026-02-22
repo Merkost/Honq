a038dbeee7ab94176d9d3fb1fade8d361d6c5f4b
merkost <konstantinlikes@gmail.com>
2026-02-22 16:15:27 +1100

    feat: update app icon filename and initialize Firebase in MainViewController

--- shared/src/iosMain/kotlin/com/merkost/honq/MainViewController.kt
+++ shared/src/iosMain/kotlin/com/merkost/honq/MainViewController.kt
@@ -4,10 +4,13 @@ import androidx.compose.ui.window.ComposeUIViewController
 import com.merkost.honq.core.logging.initCedar
 import com.merkost.honq.data.di.databaseModule
 import com.merkost.honq.di.sharedModules
+import dev.gitlive.firebase.Firebase
+import dev.gitlive.firebase.initialize
 import org.koin.compose.KoinApplication
 import platform.UIKit.UIViewController
 
 fun MainViewController(): UIViewController {
+    Firebase.initialize()
     initCedar()
     return ComposeUIViewController {
         KoinApplication(
